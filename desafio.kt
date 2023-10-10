b68ffd860de662d5798901b5330ae97c04c44037
Felipe Barros <Kazordoon@pm.me>
2023-10-09 23:32:01 -0300

    feat: Implement challenge

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,61 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(val nome: String, val nivel: Nivel) {
+	val conteudos: MutableList<ConteudoEducacional> = mutableListOf()
     val inscritos = mutableListOf<Usuario>()
     
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+    }
+    
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    
+    override fun toString(): String {
+        var conteudosTexto = ""
+        for (conteudo in conteudos) {
+            conteudosTexto += "\n\tNome: ${conteudo.nome} / "
+            conteudosTexto += "Duração: ${conteudo.duracao}m"
+        }
+        
+        var inscritosTexto = ""
+        for (inscrito in inscritos) {
+            inscritosTexto += "\n\t${inscrito.nome}"
+        }
+        
+        return """
+===================================
+Informações sobre a nossa formação
+===================================
+        
+Nome: $nome
+Nivel: $nivel
+Conteúdos: $conteudosTexto
+Estudantes inscritos: $inscritosTexto
+        """
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+	val usuario1 = Usuario("Luis")
+    val usuario2 = Usuario("Gabriel")
+    val usuario3 = Usuario("Raphaela")
+
+    var duracaoEmMinutos = 180
+    val pooConteudo = ConteudoEducacional("POO com Kotlin", duracaoEmMinutos)
+    val exceptionsConteudo = ConteudoEducacional("Tratamento de exceções com Kotlin")
+    
+    val formacao = Formacao("Curso de Kotlin", Nivel.BASICO)
+    formacao.adicionarConteudo(pooConteudo)
+    formacao.adicionarConteudo(exceptionsConteudo)
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao.matricular(usuario3)
+    
+	println(formacao)
 }
